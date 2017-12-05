4c8eff18531082f46dea76bea69f82ec4dd44686
Thorsten Bux <thorsten.bux@outlook.com>
2017-12-06 12:01:50 +1300

    add url to files

--- aws_s3.js
+++ aws_s3.js
@@ -3,18 +3,21 @@ AWS.config.region = 'us-east-1'; // Region
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: 'us-east-1:9a40d2a3-7c51-4b65-bf4d-762e3411bfca',
 });
-var bucket = new AWS.S3({params: {Bucket: 'artk-dist'}});
+const bucketName = 'artk-dist';
+const downloadBaseDir = 'download';
+const bucket = new AWS.S3({params: {Bucket: bucketName}});
+
+const urlPrefix="https://s3-us-west-2.amazonaws.com/" + bucketName + '/';
 
 bucket.listObjectsV2(function (err, data) {
     if (err) {
         document.getElementById('status').innerHTML =
         'Could not load objects from S3 error: ' + err;
     } else {
-        document.getElementById('status').innerHTML =
-        'Loaded ' + data.Contents.length + ' items from S3';
+        document.getElementById('status').innerHTML ='Loaded ' + data.Contents.length + ' items from S3';
+
         for (var i = 0; i < data.Contents.length; i++) {
-        document.getElementById('objects').innerHTML +=
-            '<li>' + data.Contents[i].Key + '</li>';
+        document.getElementById('objects').innerHTML += `<li><a href=${urlPrefix}${data.Contents[i].Key}>${data.Contents[i].Key}</a></li>`
         }
     }
 }); 
